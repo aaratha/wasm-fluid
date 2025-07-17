9ebaf1e931762ed0e0a49f7bf81d293d229fcf54
Aseem Ratha <aseemratha@gmail.com>
2025-07-17 13:42:55 -0400

    added basic mouse event interactivity

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use wasm_bindgen::prelude::*;
-use web_sys::{window, HtmlCanvasElement, WebGl2RenderingContext};
+use wasm_bindgen::JsCast;
+use web_sys::{window, HtmlCanvasElement, MouseEvent, WebGl2RenderingContext};
+
+// Store state globally if needed
+static mut GLO_CONTEXT: Option<WebGl2RenderingContext> = None;
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
@@ -12,6 +16,29 @@ pub fn start() -> Result<(), JsValue> {
 
     gl.clear_color(1.0, 0.0, 0.0, 1.0);
     gl.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
+
+    // Store context globally
+    unsafe {
+        GLO_CONTEXT = Some(gl.clone());
+    }
+
+    // Add mouse click event listener
+    let closure = Closure::wrap(Box::new(move |_event: MouseEvent| {
+        // On click, change color randomly
+        let r = js_sys::Math::random() as f32;
+        let g = js_sys::Math::random() as f32;
+        let b = js_sys::Math::random() as f32;
+
+        unsafe {
+            if let Some(ref gl) = GLO_CONTEXT {
+                gl.clear_color(r, g, b, 1.0);
+                gl.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
+            }
+        }
+    }) as Box<dyn FnMut(_)>);
+
+    canvas.add_event_listener_with_callback("click", closure.as_ref().unchecked_ref())?;
+    closure.forget(); // Leak memory intentionally to keep it alive
 
     Ok(())
 }
